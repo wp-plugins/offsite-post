4790351a90b05cdf62110346ea3f9591b60c1e06
pathawks <pathawks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 05:00:20 +0000

    Checking in some code. Not clean, so I'm just throwing this out there for now. Hope to tidy it up in a week or so.
    git-svn-id: https://plugins.svn.wordpress.org/offsite-post/trunk@441917 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ offsite-post.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: DirtySuds - 301 Link
+Plugin URI: http://dirtysuds.com
+Description: Redirects Links offsite
+Author: Pat Hawks
+Version: 1.00
+Author URI: http://www.pathawks.com
+
+Updates:
+1.00.20110226 - First Version
+
+  Copyright 2011 Pat Hawks  (email : pat@pathawks.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function dirtysuds_link_get_redirect( $permalink, $post ) {
+	preg_match('!https?://[\S]+!', $post->post_content, $newlink);
+	if (isset($newlink[0]))
+		return $newlink[0];
+	else
+		return $permalink;
+}
+
+function dirtysuds_link_filter_permalink( $permalink, $post ) {
+	if (has_post_format('link', $post))
+		return dirtysuds_link_get_redirect( $permalink, $post );
+	else
+		return $permalink;
+}
+
+function dirtysuds_link_do_redirect($single_template) {
+	global $post;
+	if (has_post_format('link', $post)) {
+		$newlink = dirtysuds_link_get_redirect( $_SERVER['REQUEST_URI'], $post );
+		if ( $newlink != $_SERVER['REQUEST_URI'])
+			header( 'Location: ' . $newlink ) ;
+		else
+			return $single_template;
+	} else
+		return $single_template;
+}
+
+add_filter('single_template', 'dirtysuds_link_do_redirect');
+add_filter('post_link', 'dirtysuds_link_filter_permalink',1,2);
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Offsite Post ===
+
+Contributors: Pat Hawks
+Tags: plugins, wordpress, post, link, 301, offsite, redirect
+Requires at least: 3.1
+Tested up to: 3.2
+Stable tag: trunk
+
+Make a post redirect to an external URL
+
+== Description ==
+
+You can use this to show posts from other sites on your homepage and in your archives alongside your own sites posts.
+After you activate the plugin, any post with the post type "Link" will redirect to the first link in that posts content.
+
+== Installation ==
+
+1. Upload 'dirtysuds-category-thumbnail' to the '/wp-content/plugins/' directory
+2. Activate **DirtySuds - Category Thumbnail** through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Who can I contact for support? =
+
+Pat Hawks is the man for that job. [pat@dirtysuds.com](mailto:pat@dirtysuds.com)
+
+== Changelog ==
+
+= 1.00 20110922 =
+* First version
+* Works
